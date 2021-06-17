ab756395b38a568c3fb267a4cd28ca4f248ae187
elmaxe <axel.elmarsson@gmail.com>
2021-06-17 16:34:51 +0200

    Don't display what people have voted until the vote has ended
    Currently drek has the ability to see what people have voted when the
    vote is open. Removed this and only display it after the voting is closed.
    
    When voting is open, '?' is displayed instead of vote numbers. In order to
    see how many people have voted, an indicator to show how many people have
    voted has also been added.

--- src/Results/Results.js
+++ src/Results/Results.js
@@ -2,16 +2,27 @@ import React, { Component } from 'react'
 
 class Results extends Component {
 	render() {
-		const {question, alternatives, votes} = this.props
+		const { question, alternatives, votes, open, users } = this.props
+
+		const numVotes = votes.reduce((a, b) => a + b, 0);
+		// user.attending is a boolean
+		// However, true + true = 2 (and so on) in Javascript, so the map is actually unnecessary
+		const attending = users.map(u => u.attending ? 1 : 0).reduce((a, b) => a + b, 0)
+
 		return (
 			<div>
 				<h1>{ question }</h1>
+				<h3>Antal som röstat: <span style={{color: "black"}}>{numVotes} / {attending}</span></h3>
 	            <ul className="results">
 	              { alternatives.map((alternative, index) => (
 	                <li key={JSON.stringify(alternative)}>
-	                  <div className="theme-color bar-label">{ alternative.content } ({ votes[index] })</div>
+	                  <div className="theme-color bar-label">{ alternative.content } { open ? "?" : votes[index] }</div>
 	                  <div className="bar-container">
-	                    <div className="bar lighten-3" style={{width: (votes.reduce((a,b) => (a+b), 0) > 0 ? (votes[index] / votes.reduce((a,b) => Math.max(a,b), 0)) : 0) * 100 + '%'}}></div>
+						{open ?
+							<div className="bar lighten-3" style={{width: "0%"}}></div>
+							:
+							<div className="bar lighten-3" style={{width: (votes.reduce((a,b) => (a+b), 0) > 0 ? (votes[index] / votes.reduce((a,b) => Math.max(a,b), 0)) : 0) * 100 + '%'}}></div>
+						}
 	                  </div>
 	                  <div className="clear"></div>
 	                </li>
